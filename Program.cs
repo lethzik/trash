76eea7dbdb2a975d0fccaed3600e4bf78780eb68
lethzik <lethzik@gmail.com>
2014-08-17 18:46:14 +0100

    Program.cs
    Brand

---
+++ Program.cs
@@ -0,0 +1,190 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+#endregion
+
+namespace Brand
+{
+    internal class Program
+    {
+        public const string ChampionName = "Brand";
+
+        //Orbwalker instance
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        //Spells
+        public static List<Spell> SpellList = new List<Spell>();
+
+        public static Spell Q;
+        public static Spell W;
+		public static Spell E;		
+        public static Spell R;
+
+        //Menu
+        public static Menu Config;
+
+        private static Obj_AI_Hero Player;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+
+            if (Player.BaseSkinName != ChampionName) return;
+
+            //Create the spells
+            Q = new Spell(SpellSlot.Q, 900);
+			W = new Spell (SpellSlot.W, 900);
+            E = new Spell(SpellSlot.E, 625);
+            R = new Spell(SpellSlot.R, 750);
+
+            Q.SetSkillshotQ.SetSkillshot(0.25f, 70f, 1800f, true, Prediction.SkillshotType.SkillshotLine);
+
+            SpellList.Add(Q);
+			SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+
+            //Create the menu
+            Config = new Menu(ChampionName, ChampionName, true);
+
+            //Orbwalker submenu
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+
+            //Add the target selector to the menu as submenu.
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            //Load the orbwalker and add it to the menu as submenu.
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            //Combo menu:
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+			Config.SubMenu("Combo").AddItem(new MenuItem)("UseWCombo", "Use W").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseRCombo", "Use R").SetValue(true));
+            Config.SubMenu("Combo")
+                .AddItem(
+                    new MenuItem("ComboActive", "Combo!").SetValue(
+                        new KeyBind(Config.Item("Orbwalk").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            //Misc
+            Config.AddSubMenu(new Menu("Misc", "Misc"));
+            Config.SubMenu("Misc").AddItem(new MenuItem("InterruptSpells", "Interrupt spells with R").SetValue(true));
+            Config.SubMenu("Misc").AddItem(new MenuItem("KillstealR", "Killsteal with R").SetValue(false));
+
+            //Drawings menu:
+            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            Config.SubMenu("Drawings")
+                .AddItem(
+                    new MenuItem("QRange", "Q Range").SetValue(new Circle(true, Color.FromArgb(150, Color.DodgerBlue))));
+            Config.SubMenu("Drawings")
+                .AddItem(
+                    new MenuItem("RRange", "R Range").SetValue(new Circle(true, Color.FromArgb(150, Color.DodgerBlue))));
+            Config.AddToMainMenu();
+
+            //Add the events we are going to use:
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+            Interrupter.OnPosibleToInterrupt += Interrupter_OnPosibleToInterrupt;
+            Game.PrintChat(ChampionName + " Loaded!");
+        }
+
+        private static void Interrupter_OnPosibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (!Config.Item("InterruptSpells").GetValue<bool>()) return;
+
+            if (Player.Distance(unit) < R.Range && R.IsReady())
+            {
+                R.Cast();
+            }
+        }
+
+        private static void Combo()
+        {
+            Orbwalker.SetAttacks(true);
+			var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Magical);
+            var qTarget = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+			var wTarget = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            var rTarget = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+			
+			bool useE = Config.Item("UseECombo").GetValue<bool>();
+            bool useQ = Config.Item("UseQCombo").GetValue<bool>();
+			bool useW = Config.Item("UseWCombo").GetValue<bool>();
+            bool useR = Config.Item("UseRCombo").GetValue<bool>();
+
+            if (eTarget !=null && useE && E.IsReady())
+            {
+                E.Cast(eTarget);
+            }
+
+            if (qTarget !=null && useQ && Q.IsReady())
+            {
+                Q.Cast(qTarget);
+            }
+			if (qTarget !=null && useW && W.IsReady())
+            {
+                if (qTarget.HasBuff("PilarOfFlame"))
+                    W.Cast();
+            }
+            if (rTarget != null && useR && R.IsReady())
+            {
+                R.Cast(rTarget);
+            }
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Player.IsDead) return;
+            Orbwalker.SetAttacks(true);
+            Orbwalker.SetMovement(true);
+            
+            var useRKS = Config.Item("KillstealR").GetValue<bool>() && R.IsReady();
+
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+            if (useRKS)
+            {
+                Killsteal();
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            //Draw the ranges of the spells.
+            foreach (var spell in SpellList)
+            {
+                var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
+                if (menuItem.Active)
+                    Utility.DrawCircle(Player.Position, spell.Range, menuItem.Color);
+            }
+        }
+        
+        private static void Killsteal()
+        {
+            
+                foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(R.Range)))
+                {
+                    if (R.IsReady() && hero.Distance(ObjectManager.Player) <= R.Range &&
+                        DamageLib.getDmg(hero, DamageLib.SpellType.R) >= hero.Health)
+                        R.Cast();
+                }
+            
+        }
+    }
+}
